676ae4c57e1a2196fd1be36ff36073a95cdfd58c
Ayush Sinha <ayush22.sinha@gmail.com>
2025-09-21 23:28:44 +0530

    Add files via upload
    Frontend Part

---
+++ run-website.bat
@@ -0,0 +1,19 @@
+@echo off
+echo GLYPH Website Runner
+echo ===================
+echo.
+echo Opening GLYPH website in your default browser...
+echo.
+start "" "index.html"
+echo.
+echo The website has been opened!
+echo.
+echo Note: Some features may not work when running from file:// protocol
+echo For full functionality, serve the files through an HTTP server.
+echo.
+echo Available files:
+echo - index.html         (Main website)
+echo - demo-test.html     (Standalone demo)
+echo - theme-test.html    (Theme testing)
+echo.
+pause
